1a8885c6b96bde28c628545ca312a012e5b3d3d2
Fernando <mr.fernanddo@gmail.com>
2025-10-19 01:24:40 -0500

    feat: add @nestjs/config and configure global settings; update health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 60
+    }]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: ThrottlerGuard }
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import helmet from 'helmet'
+//import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.use(helmet())
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(',') ?? ['http://localhost:3000'],
+    credentials: true
+  })
+
+  //app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  const port = Number(process.env.APP_PORT ?? 3000)
+  await app.listen(port)
+  console.log(`TOBI API on http://localhost:${port}`)
 }
-bootstrap();
+bootstrap()
